a31b6d0a889ecaa9c15caf18dde4e7520a8b6b5a
Josh Buker <accounts+github@joshbuker.com>
2021-08-29 16:44:05 -0700

    Copy paste work in progress

--- main.py
+++ main.py
@@ -1,9 +1,90 @@
-def move_backward():
-    pass
-def move_forward():
-    pass
-joystickbit.init_joystick_bit()
+RADIO_GROUP = 90
+LEFT = 0
+RIGHT = 1
+FORWARD = 0
+BACKWARD = 1
 
-def on_forever():
-    pass
-basic.forever(on_forever)
+def setup():
+  basic.show_icon(IconNames.HEART)
+  radio.set_group(RADIO_GROUP)
+  radio.on_received_value(receive_message)
+  joystickbit.init_joystick_bit()
+  joystickbit.on_button_event(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.DOWN, turn_left_90)
+  joystickbit.on_button_event(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.DOWN, turn_right_90)
+  joystickbit.on_button_event(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.DOWN, move_forward)
+  joystickbit.on_button_event(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.DOWN, move_backward)
+  basic.clear_screen()
+
+# def calibrate():
+
+# def loop():
+
+def send_message(key, value):
+  radio.send_value(key, value)
+
+def receive_message(key, value):
+  if key == 'turning':
+    show_turning(value)
+  elif key == 'moving':
+    show_moving(value)
+  elif key == 'done':
+    basic.clear_screen()
+
+def show_turning(value):
+  if value == LEFT:
+    basic.show_leds("""
+                    . . # . .
+                    . # . . .
+                    # # # # #
+                    . # . . .
+                    . . # . .
+                    """)
+  elif value == RIGHT:
+    basic.show_leds("""
+                    . . # . .
+                    . . . # .
+                    # # # # #
+                    . . . # .
+                    . . # . .
+                    """)
+
+def show_moving(value):
+  if value == FORWARD:
+    basic.show_leds("""
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    """)
+  elif value == BACKWARD:
+    basic.show_leds("""
+                    . . # . .
+                    . . # . .
+                    # . # . #
+                    . # # # .
+                    . . # . .
+                    """)
+
+def move_forward():
+  basic.show_icon(IconNames.DIAMOND)
+  send_message('move', FORWARD)
+  basic.clear_screen()
+
+def move_backward():
+  basic.show_icon(IconNames.DIAMOND)
+  send_message('move', BACKWARD)
+  basic.clear_screen()
+
+def turn_right_90():
+  basic.show_icon(IconNames.DIAMOND)
+  send_message('turn', RIGHT)
+  basic.clear_screen()
+
+def turn_left_90():
+  basic.show_icon(IconNames.DIAMOND)
+  send_message('turn', LEFT)
+  basic.clear_screen()
+
+setup()
+# basic.forever(loop)
--- main.ts
+++ main.ts
@@ -1,12 +1,91 @@
-function move_backward() {
+let RADIO_GROUP = 90
+let LEFT = 0
+let RIGHT = 1
+let FORWARD = 0
+let BACKWARD = 1
+function setup() {
+    basic.showIcon(IconNames.Heart)
+    radio.setGroup(RADIO_GROUP)
+    radio.onReceivedValue(function receive_message(key: string, value: number) {
+        if (key == "turning") {
+            show_turning(value)
+        } else if (key == "moving") {
+            show_moving(value)
+        } else if (key == "done") {
+            basic.clearScreen()
+        }
+        
+    })
+    joystickbit.initJoystickBit()
+    joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function turn_left_90() {
+        basic.showIcon(IconNames.Diamond)
+        send_message("turn", LEFT)
+        basic.clearScreen()
+    })
+    joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function turn_right_90() {
+        basic.showIcon(IconNames.Diamond)
+        send_message("turn", RIGHT)
+        basic.clearScreen()
+    })
+    joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function move_forward() {
+        basic.showIcon(IconNames.Diamond)
+        send_message("move", FORWARD)
+        basic.clearScreen()
+    })
+    joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function move_backward() {
+        basic.showIcon(IconNames.Diamond)
+        send_message("move", BACKWARD)
+        basic.clearScreen()
+    })
+    basic.clearScreen()
+}
+
+//  def calibrate():
+//  def loop():
+function send_message(key: string, value: number) {
+    radio.sendValue(key, value)
+}
+
+function show_turning(value: number) {
+    if (value == LEFT) {
+        basic.showLeds(`
+                    . . # . .
+                    . # . . .
+                    # # # # #
+                    . # . . .
+                    . . # . .
+                    `)
+    } else if (value == RIGHT) {
+        basic.showLeds(`
+                    . . # . .
+                    . . . # .
+                    # # # # #
+                    . . . # .
+                    . . # . .
+                    `)
+    }
     
 }
 
-function move_forward() {
+function show_moving(value: number) {
+    if (value == FORWARD) {
+        basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+    } else if (value == BACKWARD) {
+        basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    # . # . #
+                    . # # # .
+                    . . # . .
+                    `)
+    }
     
 }
 
-joystickbit.initJoystickBit()
-basic.forever(function on_forever() {
-    
-})
+setup()
